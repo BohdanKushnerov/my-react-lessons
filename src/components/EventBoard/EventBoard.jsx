7cff3ea5f942c93243ec6d72d8ae824ba00b333a
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-03-14 14:54:24 +0200

    01-lesson

---
+++ src/components/EventBoard/EventBoard.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import {Event} from "components/Event/Event";
+import css from "./EventBoard.module.css";
+
+export const EventBoard = ({events}) => {
+  return (
+  <div className={css.eventBoard}>
+    {events.map(event => (
+      <Event 
+        key={event.name} 
+        name={event.name} 
+        location={event.location} 
+        speaker={event.speaker}
+        type={event.type}
+        start={event.time.start}
+        end={event.time.end}
+      />
+    ))}
+  </div>
+  )
+}
+
+EventBoard.propTypes = {
+  events: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      speaker: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      time: PropTypes.exact({
+        start: PropTypes.string.isRequired,
+        end: PropTypes.string.isRequired,
+    }),
+    })
+  )
+}
