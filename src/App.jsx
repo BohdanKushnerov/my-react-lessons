135e760cb5ad27c55d30e5ec7122fdd6c2e0802d
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-04-25 13:13:45 +0300

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useSelector, useDispatch } from 'react-redux';
+//================COUNTER=====================================
+// import { useSelector, useDispatch } from 'react-redux';
 
-import { increment, decrement } from './redux/store';
+// import { increment, decrement } from './redux/store';
+
+// export const App = () => {
+//   const value = useSelector(state => state.myValue);
+
+//   const dispatch = useDispatch();
+//   return (
+//     <>
+//       <button onClick={() => dispatch(decrement(1))}>Decrement</button>
+//       <div>Counter: {value}</div>
+//       <button onClick={() => dispatch(increment(1))}>Increment</button>
+//     </>
+//   );
+// };
+
+//==========================================================
+// import { useSelector, useDispatch } from 'react-redux';
+import { TaskCounter } from './components/TaskCounter/TaskCounter';
+import { TaskList } from './components/TaskList/TaskList';
+import { TaskForm } from './components/TaskForm/TaskForm';
+import { StatusFilter } from './components/StatusFilter/StatusFilter';
 
 export const App = () => {
-  const value = useSelector(state => state.myValue);
+  // const value = useSelector(state => state.myValue);
+  // const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
   return (
     <>
-      <button onClick={() => dispatch(decrement(1))}>Decrement</button>
-      <div>Counter: {value}</div>
-      <button onClick={() => dispatch(increment(1))}>Increment</button>
+      <TaskCounter></TaskCounter>
+      <StatusFilter></StatusFilter>
+      <TaskForm></TaskForm>
+      <TaskList></TaskList>
     </>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,54 @@
+//======================COUNTER===========================
+// import { configureStore } from '@reduxjs/toolkit';
+// import { createAction, createReducer } from '@reduxjs/toolkit';
+
+// export const increment = createAction('myValue/increment');
+// export const decrement = createAction('myValue/decrement');
+
+// const myReducer = createReducer(0, {
+//   [increment]: (state, action) => state + action.payload,
+//   [decrement]: (state, action) => state - action.payload,
+// });
+
+// export const store = configureStore({
+//   reducer: {
+//     myValue: myReducer,
+//   },
+// });
+
+//=======================================================
+
+// import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+import { rootReducer } from './reducer';
 
-export const increment = createAction('myValue/increment');
-export const decrement = createAction('myValue/decrement');
+// Початкове значення стану Redux для кореневого редюсера,
+// якщо не передати параметр preloadedState.
+// const initialState = {
+//   tasks: [
+//     { id: 0, text: 'Learn HTML and CSS', completed: true },
+//     { id: 1, text: 'Get good at JavaScript', completed: true },
+//     { id: 2, text: 'Master React', completed: false },
+//     { id: 3, text: 'Discover Redux', completed: false },
+//     { id: 4, text: 'Build amazing apps', completed: false },
+//   ],
+//   filters: {
+//     status: 'all',
+//   },
+// };
 
-const myReducer = createReducer(0, {
-  [increment]: (state, action) => state + action.payload,
-  [decrement]: (state, action) => state - action.payload,
-});
+// Поки що використовуємо редюсер який
+// тільки повертає отриманий стан
+// const rootReducer = (state = initialState, action) => {
+//   return state;
+// };
+
+// Створюємо розширення стора, щоб додати інструменти розробника
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
 
 export const store = configureStore({
-  reducer: {
-    myValue: myReducer,
-  },
+  reducer: rootReducer,
+  devTools: true,
 });
